ddec3cf057a77bc6276f7cb7d70432e4695046a3
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-07 01:50:19 -0700

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css"
 
 function App() {
-  return <div className="App">frontend with reactttttt</div>
+  return <div className="App">frontend with rrrreactttttt</div>
 }
 
 export default App
