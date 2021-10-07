6cbc32536393f6fb67b1f49a33c46749bdf7cfa7
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-07 04:39:23 -0700

    006

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import LandingPage from "./screens/LandingPage/LandingPage"
 
-function App() {
-  return <div className="App">frontend pageeeee</div>
-}
+const App = () => (
+  <>
+    <Header />
+
+    <LandingPage />
+    <Footer />
+  </>
+)
 
 export default App
---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,37 @@
+import { Container, Row, Button } from "react-bootstrap"
+import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">welcome to CosNoteBook</h1>
+              <p className="subtitle">one safe place for all your notes</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  register
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default LandingPage
