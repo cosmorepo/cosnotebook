576acdcbc2563f9159823883090bfd06456f15e7
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-13 23:07:06 -0700

    010_33

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,27 @@ const express = require("express")
 const notes = require("./data/notes")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes")
+const { errorHandler, notFound } = require("./middlewares/errorMiddleware")
 
 const app = express()
 dotenv.config()
 connectDB()
 
-app.get("/", (req, res) => {
-  res.send("api is runningggdfgdf...")
-})
+app.use(express.json())
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes)
-})
+// app.get("/", (req, res) => {
+//   res.send("api is runningggdfgdf...")
+// })
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id)
-  res.send(note)
-})
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes)
+// })
+
+app.use("/api/users", userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
