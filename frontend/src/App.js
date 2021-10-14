65c92f046c64630192eb2cebfd5a26762fd7a992
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-14 01:25:17 -0700

    010_50

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,17 @@ import Footer from "./components/Footer/Footer"
 import LandingPage from "./screens/LandingPage/LandingPage"
 import { BrowserRouter, Route } from "react-router-dom"
 import MyNotes from "./screens/MyNotes/MyNotes"
+import LoginScreen from "./screens/LoginScreen/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen"
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
-      <Route path="/" exact component={LandingPage} />
+      <Route path="/" exact component={LandingPage} exact />
+      <Route path="/login" exact component={LoginScreen} />
+      <Route path="/register" exact component={RegisterScreen} />
+
       <Route path="/mynotes" component={MyNotes} />
     </main>
 
