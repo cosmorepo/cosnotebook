8f8dd950b8afc86aa50b567e64168a082aa520c0
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-16 05:14:36 -0700

    011_25

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -9,9 +9,23 @@ import {
   Button,
   Brand,
 } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from "../../actions/userActions"
 
 const Header = () => {
   const history = useHistory()
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+    history.push("/")
+  }
+
+  //useEffect(() => {}, [userInfo]);
 
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
@@ -38,12 +52,7 @@ const Header = () => {
               <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item
-                onClick={() => {
-                  localStorage.removeItem("userInfo")
-                  history.push("/")
-                }}
-              >
+              <NavDropdown.Item onClick={logoutHandler}>
                 Log Out
               </NavDropdown.Item>
             </NavDropdown>
