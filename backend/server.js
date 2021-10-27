fee643d1cc52a9145ae25c0b34d03380fc6a9c64
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-27 03:25:56 -0700

    one more try baby

--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,10 @@ app.use("/api/notes", noteRoutes)
 
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")))
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(
-        __dirname,
-        "../frontend/build",
-        "../build",
-        "../build/index.html"
-      )
-    )
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   })
 } else {
   app.get("/", (req, res) => {
